88ebdd3cc1acfb1039cd7f2ea2d7c532625c1c0d
bdonauba <bdonauba@gmail.com>
2018-12-06 12:40:35 +0100

    session scoped access password

--- src/main/groovy/de/dumischbaenger/GroovyUserView.groovy
+++ src/main/groovy/de/dumischbaenger/GroovyUserView.groovy
@@ -1,16 +1,16 @@
 package de.dumischbaenger;
 
-import java.io.Serializable;
-import java.util.Date;
-
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped
+import javax.faces.context.FacesContext
+import javax.servlet.http.HttpSession
 
-
+@SessionScoped
 @ManagedBean(name = "groovyUserView")
 public class GroovyUserView implements Serializable {
 
-  private String  name  = "Groovy xxxx John";
+  private count=0;
 
   public GroovyUserView() {
     super();
@@ -21,11 +21,20 @@ public class GroovyUserView implements Serializable {
   }
 
   public String getName() {
-    return name + " " + new Date();
+    //    HttpServletRequest request = (HttpServletRequest) (FacesContext.getCurrentInstance().getExternalContext().getRequest());
+    //    Object username=request.getAttribute("de.dumischbaenger.jetty.username");
+    //    Object password=request.getAttribute("de.dumischbaenger.jetty.password");
+
+    HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+    Object username=session.getAttribute("de.dumischbaenger.jetty.username");
+    Object password=session.getAttribute("de.dumischbaenger.jetty.password");
+
+    String result= count++ + " " +  username + " " + new Date() + " " + password.toString().charAt(0);
+
+    return result;
   }
 
   public void setName(String name) {
     this.name = name;
   }
-
 }
